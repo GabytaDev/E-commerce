3f31320297664776634acd0b3c4a0f7c3836ed26
GabytaDev <gabyta.loyola@gmail.com>
2021-10-15 18:05:17 -0300

    agrego funcion abrir y cerrar menu mobile

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+//abrir y cerrar menu mobile
+const iconMenuMobile = document.querySelector("#icon-menu-mobile")
+const menuOpenMobile = document.querySelector(".content-menu-mobile")
+const iconCloseMenuMobile = document.querySelector(".fa-times")
+
+iconMenuMobile.onclick = ()=>{
+    menuOpenMobile.style.display = "block"
+}
+iconCloseMenuMobile.onclick = ()=> {
+    menuOpenMobile.style.display = "none"
+}
 // abrir modal carrito
 
 const iconShoppingCart = document.querySelector("#icon-shopping-cart")
@@ -7,3 +18,22 @@ iconShoppingCart.onclick = ()=>{
     modalShoppingCart.classList.toggle("ocultar")
     
 }
+
+
+/*
+const desktop = window.matchMedia('(max-width: 1025px)');
+
+function desktop_responsive(){ 
+     if(desktop.matches){ 
+         $('body').css('background','darkblue'); } 
+} 
+desktop_responsive(desktop); 
+desktop.addEventListener(desktop_responsive); 
+
+const mobile = window.matchMedia('(max-width: 375px)'); 
+function mobile_responsive(){ 
+    if (mobile.matches){
+         $('body').css('background','darkorange'); } 
+} 
+mobile_responsive(mobile); 
+mobile.addEventListener(mobile_responsive);*/
